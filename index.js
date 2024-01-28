6ae3e487b94fc0a2df4331c18f4a022a1f5fb4d2
i am just learner <fikri2701@gmail.com>
2024-01-28 16:08:50 +0000

    REDUX toolkit with asynchronous function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const icecreamActions =
   require("./features/icecream/icecreamSlice").icecreameActions;
+const fetchUser = require("./features/user/userSlice").fetchUsers;
 
 console.log("Initial State", store.getState());
 
@@ -9,13 +10,14 @@ const unsubscribe = store.subscribe(() => {
   console.log("Updated State", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
 
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(3));
-store.dispatch(icecreamActions.restocked(3));
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(3));
+// store.dispatch(icecreamActions.restocked(3));
 
-unsubscribe();
+store.dispatch(fetchUser());
+// unsubscribe();
