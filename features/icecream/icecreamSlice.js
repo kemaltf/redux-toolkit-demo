2dff81e0faaedf636b875e4dc7ccd051c6a614c6
i am just learner <fikri2701@gmail.com>
2024-01-28 15:14:34 +0000

    create extra reducers

--- features/icecream/icecreamSlice.js
+++ features/icecream/icecreamSlice.js
@@ -1,4 +1,5 @@
 const createSlice = require("@reduxjs/toolkit").createSlice;
+const { cakeActions } = require("../cake/cakeSlice");
 
 const initialState = {
   numOfIcecreams: 5,
@@ -14,6 +15,11 @@ const icecreamSlice = createSlice({
     restocked: (state, action) => {
       state.numOfIcecreams += action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(cakeActions.ordered, (state) => {
+      state.numOfIcecreams--;
+    });
   },
 });
 
--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const icecreamActions =
 
 console.log("Initial State", store.getState());
 
-const unsubscribe = store.subscribe(() => {});
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated State", store.getState());
+});
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
